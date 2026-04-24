8ade40d90f8eaf7a00853fd350bc52885ca9779b
Annie Zhang <148919342+annieeeee04@users.noreply.github.com>
2026-04-24 15:45:00 -0700

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.py
@@ -0,0 +1,29 @@
+class TimeMap:
+
+    def __init__(self):
+        self.cache = {} # key -> [timestamp, value]
+
+    # Time: O(1)
+    # Space: O(n)
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.cache:
+            self.cache[key] = []
+        self.cache[key].append((timestamp, value))
+
+    # Time: O(log n)
+    # Space: O(1)
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.cache:
+            return ""
+        
+        arr = self.cache[key]
+        l, r = 0, len(arr)-1
+        ans = ""
+        while l <= r:
+            mid = (l + r) // 2
+            if arr[mid][0] <= timestamp:
+                ans = arr[mid][1]
+                l = mid + 1
+            else:
+                r = mid - 1
+        return ans
