8c95c465b9c1a47734dde627a373cbbd2e55f0e4
Annie Zhang <148919342+annieeeee04@users.noreply.github.com>
2026-04-24 13:35:41 -0700

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.py
@@ -0,0 +1,20 @@
+# Time: O(n log n)
+# Space: O(n)
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        # traverse in the reverse order, 
+        # the we go through cars from the one closest to the target
+        # which is most likely to reach the target the first
+        # thus we can identify the fleet correctly
+        cars = sorted(zip(position, speed), reverse = True)
+        stack = []
+
+        for p, s in cars:
+            time = (target - p) / s
+            stack.append(time)
+
+            if len(stack) > 1 and stack[-1] <= stack[-2]:
+                stack.pop()
+
+        return len(stack)
