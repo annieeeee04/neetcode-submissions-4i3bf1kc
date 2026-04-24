1605c1fb13fe2cba03e7c87a9b561292b28e5f63
Annie Zhang <148919342+annieeeee04@users.noreply.github.com>
2026-04-24 14:38:05 -0700

    Add: eating-bananas - submission-4

---
+++ Data Structures & Algorithms/eating-bananas/submission-4.py
@@ -0,0 +1,20 @@
+# Time: O(k log k), k = max_speed
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def eatTime(s):
+            time = 0
+            for p in piles:
+                time += (p + s - 1) // s
+            return time
+        
+        l, r = 1, max(piles)
+        while l < r:
+            mid = (l + r) // 2
+            time = eatTime(mid)
+            
+            if time <= h:
+                r = mid
+            else:
+                l = mid + 1
+        return l
