04b2da67e77efb550cdfa122dcf30e3a5059843f
Annie Zhang <148919342+annieeeee04@users.noreply.github.com>
2026-04-26 01:27:44 -0700

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.py
@@ -0,0 +1,26 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        graph1, graph2 = {}, {}
+        m, n = len(s1), len(s2)
+
+        for char in s1:
+            graph1[char] = graph1.get(char, 0) + 1
+        
+        for char in s2[:m]:
+            graph2[char] = graph2.get(char, 0) + 1
+        
+        if graph1 == graph2: return True
+
+        for i in range(m, n):
+            left = i - m
+            last = s2[left]
+            graph2[last] -= 1
+            if graph2[last] == 0:
+                del graph2[last]
+            
+            new = s2[i]
+            graph2[new] = graph2.get(new, 0) + 1
+            if graph1 == graph2:
+                return True
+        
+        return False
