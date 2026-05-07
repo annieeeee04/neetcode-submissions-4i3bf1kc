6440f30fe8ff13fc50d66c4765ad3ba17ddc221d
Annie Zhang <148919342+annieeeee04@users.noreply.github.com>
2026-05-07 00:20:57 -0700

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.py
@@ -0,0 +1,25 @@
+# BFS
+# Time Complexity: O(N+E)
+# Space Complexity: O(N+E)
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        graph = {i:[] for i in range(n)}
+        for u,v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+        
+        visit = set()
+        visit.add(0)
+        q = deque([[0,-1]])
+
+        while q:
+            i, prev = q.popleft()
+            for e in graph[i]:
+                if e == prev:
+                    continue
+                if e in visit:
+                    return False
+                q.append((e,i))
+                visit.add(e)
+        
+        return len(visit) == n
