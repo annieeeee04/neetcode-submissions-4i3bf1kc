c01d33123923cfc03935ff72e9ad205eb4ef90f7
Annie Zhang <148919342+annieeeee04@users.noreply.github.com>
2026-05-09 18:44:59 -0700

    Add: non-cyclical-number - submission-2

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def sumSquares(n):
+            return sum(int(d) ** 2 for d in str(n))
+        
+        slow, fast = n, sumSquares(n)
+        # With OR, both conditions would need to be False to exit
+        while fast != 1 and slow != fast:
+            slow = sumSquares(slow)
+            fast = sumSquares(sumSquares(fast))
+        
+        return fast == 1
