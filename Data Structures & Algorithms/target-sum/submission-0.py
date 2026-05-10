67f15d1573d877377be66ef607be2d15224d6445
Annie Zhang <148919342+annieeeee04@users.noreply.github.com>
2026-05-10 13:41:48 -0700

    Add: target-sum - submission-0

---
+++ Data Structures & Algorithms/target-sum/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        cache = {} # value -> ways to achieve
+        cache[0] = 1
+
+        for n in nums:
+            next_cache = {}
+            for total, ways in cache.items():
+                next_cache[total+n] = next_cache.get(total+n, 0) + ways
+                next_cache[total-n] = next_cache.get(total-n, 0) + ways
+            cache = next_cache
+        return cache.get(target, 0)
