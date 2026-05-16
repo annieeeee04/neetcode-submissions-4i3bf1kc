4d89dacfb26ded469ce265e20d0fcbdec9360eb2
Annie Zhang <148919342+annieeeee04@users.noreply.github.com>
2026-05-15 22:08:40 -0700

    Add: jump-game-ii - submission-4

---
+++ Data Structures & Algorithms/jump-game-ii/submission-4.py
@@ -0,0 +1,16 @@
+# Time: O(n)
+# Space: O(1)
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        l, r = 0, 0
+        farthest = 0
+        n = len(nums)
+        res = 0
+
+        while r < n - 1:
+            for i in range(l, r+1):
+                farthest = max(farthest, nums[i] + i)
+            l, r = r+1, farthest
+            res += 1
+        return res
