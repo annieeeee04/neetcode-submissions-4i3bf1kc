59da190298618c4eb29ac6d2e6c94ff3dd76c6e3
Annie Zhang <148919342+annieeeee04@users.noreply.github.com>
2026-05-15 20:56:12 -0700

    Add: cheapest-flight-path - submission-10

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-10.py
@@ -0,0 +1,33 @@
+# Time: O()
+# Space: O()
+
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        # in each stop, we are making decisions of where to go
+        # so cache can help reducing the duplicate path
+        cache = {}
+        graph = defaultdict(list)
+        for u, v, p in flights:
+            graph[u].append((v,p))
+
+        def dfs(i, stop):
+            # start == end -> 0 travel needed
+            if i == dst:
+                return 0
+            # cannot travel anymore but didn't reach dst
+            if stop == 0:
+                return float('inf') # flag of failure
+            
+            if (i, stop) in cache:
+                return cache[(i,stop)]
+            
+            min_cost = float('inf')
+            for neighbor, p in graph[i]:
+                res = dfs(neighbor, stop - 1)
+                if res != float('inf'):
+                    min_cost = min(min_cost, res + p)
+            cache[(i, stop)] = min_cost
+            return cache[(i, stop)]
+        
+        res = dfs(src, k+1)
+        return res if res != float('inf') else -1
